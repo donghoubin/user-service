1dd3905a5eb59440821450d30d3110c48e595e68
houbin.dong <dhoubin@cn.ibm.com>
2019-12-08 19:18:34 +0800

    add log

--- src/main/java/com/mike/user/controller/UserController.java
+++ src/main/java/com/mike/user/controller/UserController.java
@@ -3,6 +3,8 @@ package com.mike.user.controller;
 import com.mike.user.entity.User;
 import com.mike.user.model.UserActiveResponse;
 import com.mike.user.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * @Description:
@@ -21,24 +22,29 @@ import java.util.Optional;
 @Controller
 public class UserController {
 
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
     @GetMapping("/userInfo/{id}")
     public ResponseEntity<User> getUser(@PathVariable("id") long id) {
         User user = userService.findUserById(id);
-        return new ResponseEntity<>(user,HttpStatus.OK);
+        log.info("Get user successfully!");
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PostMapping("/user")
     public ResponseEntity<Void> createUser(@RequestBody User user) {
         userService.createUser(user);
+        log.info("Create user successfully!");
         return new ResponseEntity<Void>(HttpStatus.CREATED);
     }
 
     @PutMapping("/user")
     public ResponseEntity<Void> editUser(@RequestBody User user) {
         userService.editUser(user);
+        log.info("Edit user successfully!");
         return new ResponseEntity<Void>(HttpStatus.CREATED);
     }
 
@@ -46,11 +52,13 @@ public class UserController {
     public ResponseEntity<UserActiveResponse> activeUser(@PathVariable("id") long id) {
         UserActiveResponse userActiveResponse = new UserActiveResponse();
         try {
+            log.info("Active user successfully!");
             userActiveResponse.setMessage("Active successfully");
             userActiveResponse.setResponseState("success");
             userService.activeUser(id);
         } catch (Exception e) {
             e.printStackTrace();
+            log.error("Fail to active user" + e.getMessage());
             userActiveResponse.setMessage("Fail to active");
             userActiveResponse.setResponseState("fail");
         }
